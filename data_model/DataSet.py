120178991df20118fcdc3026489dba78462a5ced
DongDongJu <commisori28@gmail.com>
2017-05-24 22:41:55 +0900

    dongjoo : modify model file, pre processing file and add ignore

--- data_model/DataSet.py
+++ data_model/DataSet.py
@@ -1,21 +1,23 @@
-class Day:
-	def __init__(self,site,day,hour,pm):
+class Data:
+	def __init__(self,site,detail_site,year,month,day,pm10,pm25="-"):
 		self.site=site
-		self.day=day
-		self.hour=hour
-		self.pm=pm
-
-class Month:
-	def __init__(self,month,day_list):
-		self.month=month
-		self.day_list=day_list
-
-class Year:
-	def __init__(self,year,month_list):
+		self.detail_site=detail_site
 		self.year=year
-		self.month_list=month_list
+		self.month=month
+		self.day=day
+		self.pm10=pm10
+		self.pm25=pm25
+	def __repr__(self):
+		re=self.site+" "+self.detail_site+" "+self.year+" "+self.month+" "+self.day+" "+self.pm10+" "+self.pm25
+		return re 
 
 class DataSet:
-	def __init__(self,site,year_list):
-		self.year_list=year_list
-		self.site=site
+	def __init__(self):
+		self.data_list=[]
+
+	def Add_Data(self,Data):
+		self.data_list.append(Data)
+
+	def Print_Data(self):
+		for data in self.data_list:
+			print(data)
--- pre_for_work.py
+++ pre_for_work.py
@@ -10,15 +10,39 @@ def Csv_Reader(file_path):
 		reader=csv.reader(datafile,delimiter=',')
 		for row in reader:
 			raw_data.append(row)
+	raw_data.pop(0)
 	return raw_data
+def Check_Data(row):
+	year=row[2][:4]
+	month=row[2][4:6]
+	day=row[2][6:8]
+	pm10=row[7]
+	pm25=row[8]
+	if year[0] != "2": # 2014
+		year=row[3][:4]
+		month=row[3][4:6]
+		day=row[3][6:8]
+		pm10=row[8]
+		pm25="-"
+	return year,month,day,pm10,pm25
 
 class South_Korea_DataSet:
 	rawdata_list=["data/South_korea/2014/2014_01.csv","data/South_korea/2014/2014_02.csv","data/South_korea/2014/2014_03.csv","data/South_korea/2014/2014_04.csv",
 				  "data/South_korea/2015/2015_01.csv","data/South_korea/2015/2015_02.csv","data/South_korea/2015/2015_03.csv","data/South_korea/2015/2015_04.csv",
-				  "data/South_korea/2016/2016_01.csv","data/South_korea/2016/2016_03.csv","data/South_korea/2016/2016_04.csv"] # missed 2016_02
+				  "data/South_korea/2016/2016_01.csv","data/South_korea/2016/2016_03.csv","data/South_korea/2016/2016_02.csv"] # missed 2016_04
 	def __init__(self):
+		self.data_set=DS.DataSet()
 		for rawdata in South_Korea_DataSet.rawdata_list:
 			raw_data=Csv_Reader(rawdata)
+			for row in raw_data: # each row in data files ex) 서울,중구,2015010101,0.006,0.6,0.022,0.011,44,7,서울 중구 덕수궁길 15
+				year,month,day,pm10,pm25=Check_Data(row)
+				if year == "2014": # 2014
+					data=DS.Data(row[0],row[1],year,month,day,pm10) # site,detail_site,year,month,day,pm
+				else:
+					data=DS.Data(row[0],row[1],year,month,day,pm10,pm25)
+				self.data_set.Add_Data(data)
+				if year == "2015":
+					print(data)
 			print(rawdata+" file init compelete!")
 
 if __name__ == "__main__":
