1a40980826192a96b0cdd6328041de235557c955
dongwoLee <dongwoo4553@gmail.com>
2017-05-31 17:28:48 +0900

    padding

--- pre_for_work.py
+++ pre_for_work.py
@@ -2,12 +2,13 @@ import sys
 sys.path.insert(0,"./data_model")		# for dataSet class
 import csv
 import os
-import DataSet as DS
+import data_model.DataSet as DS
+import csv
 
 def Csv_Reader(file_path):
 	raw_data=[]
 	with open(file_path,'r') as datafile:
-		reader=csv.reader(datafile,delimiter=',')
+		reader=csv.reader(datafile,delimiter='\t')
 		for row in reader:
 			raw_data.append(row)
 	raw_data.pop(0)
@@ -25,22 +26,48 @@ def Check_Data_Korea(row):
 		pm10=row[8]
 		pm25="-"
 	return year,month,day,pm10,pm25
+
+def checkNull(rawData):
+	for raw in rawData:
+		if raw[8] == '':
+			year = raw[2][:4]
+			month = raw[2][4:6]
+			day = raw[2][6:8]
+			pm10 = raw[7]
+			pm25 = raw[8]
+			sum = 0
+			cnt = 0
+			city = raw[0]
+			borough = raw[1]
+			for data in rawData:
+				if (city == data[0] and borough == data[1] and year == data[2][:4] and month == data[2][4:6] and day == data[2][6:8]):
+					if data[8] != '':
+						sum += int(data[8])
+						cnt += 1
+			raw[8] = str(sum / cnt)
+
+
 def Check_Data_China(row):
-        year=row[3]
-        month=row[4]
-        day=row[5]
-        pm10="-"
-        pm25=row[7]
-        return year,month,day,pm10,pm25
+    year=row[3]
+    month=row[4]
+    day=row[5]
+    pm10="-"
+    pm25=row[7]
+
+    return year,month,day,pm10,pm25
 
 class South_Korea_DataSet:
-	rawdata_list=["data/South_korea/2014/2014_01.csv","data/South_korea/2014/2014_02.csv","data/South_korea/2014/2014_03.csv","data/South_korea/2014/2014_04.csv",
+	'''rawdata_list=["data/South_korea/2014/2014_01.csv","data/South_korea/2014/2014_02.csv","data/South_korea/2014/2014_03.csv","data/South_korea/2014/2014_04.csv",
 				  "data/South_korea/2015/2015_01.csv","data/South_korea/2015/2015_02.csv","data/South_korea/2015/2015_03.csv","data/South_korea/2015/2015_04.csv",
-				  "data/South_korea/2016/2016_01.csv","data/South_korea/2016/2016_02.csv","data/South_korea/2016/2016_03.csv"] # missed 2016_04
+				  "data/South_korea/2016/2016_01.csv","data/South_korea/2016/2016_02.csv","data/South_korea/2016/2016_03.csv"] # missed 2016_04'''
+
+	rawdata_list = ["data/South_korea/2015/2015_01.csv", "data/South_korea/2015/2015_02.csv",
+					"data/South_korea/2015/2015_03.csv", "data/South_korea/2015/2015_04.csv"]
 	def __init__(self):
 		self.data_set=DS.DataSet()
 		for rawdata in South_Korea_DataSet.rawdata_list:
 			raw_data=Csv_Reader(rawdata)
+			checkNull(raw_data)
 			for row in raw_data: # each row in data files ex) 서울,중구,2015010101,0.006,0.6,0.022,0.011,44,7,서울 중구 덕수궁길 15
 				year,month,day,pm10,pm25=Check_Data_Korea(row)
 				if year == "2014": # 2014
@@ -48,6 +75,7 @@ class South_Korea_DataSet:
 				else:
 					data=DS.Data(row[0],row[1],year,month,day,pm10,pm25)
 				self.data_set.Add_Data(data)
+				print (data)
 			print(rawdata+" file init compelete!")
 
 class China_DataSet:
